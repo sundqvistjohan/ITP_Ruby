9faf913e3df5d713205d0f9cf01a722d1245b30e
Johan <sundqvist.johan@outlook.com>
2019-10-22 13:21:53 +0200

    Created my_group.rb

---
+++ my_group.rb
@@ -0,0 +1,4 @@
+person_1 = {name: "Johan", gender: "male", age: 31}
+person_2 = {name: "Johanna", gender: "female", age: 27}
+person_3 = {name: "Johannes", gender: "male", age: 43}
+my_group = [person_1, person_2, person_3]
