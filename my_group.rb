ba4362da175032fbb63d4266ae98614664d9ee2c
Johan <sundqvist.johan@outlook.com>
2019-10-22 13:48:15 +0200

    Added information output

--- my_group.rb
+++ my_group.rb
@@ -2,3 +2,7 @@ person_1 = {name: "Johan", gender: "male", age: 31}
 person_2 = {name: "Johanna", gender: "female", age: 27}
 person_3 = {name: "Johannes", gender: "male", age: 43}
 my_group = [person_1, person_2, person_3]
+
+my_group.each do |person|
+    puts "#{person[:name]} is a #{person[:age]} year old #{person[:gender]}"
+end
